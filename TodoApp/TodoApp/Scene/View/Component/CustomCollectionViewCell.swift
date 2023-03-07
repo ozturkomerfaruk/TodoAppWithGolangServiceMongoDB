758160f4f434b546170e8b67f493b54c253f3022
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2023-03-07 16:57:50 +0300

    HomePage UI Design

---
+++ TodoApp/TodoApp/Scene/View/Component/CustomCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCollectionViewCell.swift
+//  TodoApp
+//
+//  Created by Ömer Faruk Öztürk on 7.03.2023.
+//
+
+import UIKit
+
+final class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var taskImageView: UIImageView!
+    @IBOutlet private weak var taskTitle: UILabel!
+    @IBOutlet private weak var taskDate: UILabel!
+    
+    func configureCustomTableViewCell() {
+        taskImageView.image = UIImage(systemName: "person")
+        taskTitle.text = "Web Development"
+        taskDate.text = "09:00 AM - 11:00 AM"
+    }
+}
